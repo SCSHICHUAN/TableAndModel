7e18dace19d60e03b7c75ec9a553ae5dae137510
SHICHUAN <573387383@QQ.COM>
2018-03-27 11:42:48 +0800

    英雄联盟列表搞定

---
+++ TableAndModel/HeroModel.swift
@@ -0,0 +1,13 @@
+//
+//  HeroModel.swift
+//  TableAndModel
+//
+//  Created by SHICHUAN on 2018/3/27.
+//  Copyright © 2018年 SHICHUAN. All rights reserved.
+//
+
+import UIKit
+
+class HeroModel: NSObject {
+
+}
